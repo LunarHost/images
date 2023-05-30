f0a3345b838ba8a4e05ac0eff308cb14312c4993
AshiePleb <38188828+AshiePleb@users.noreply.github.com>
2023-05-30 23:22:43 +0100

    More testing

--- generic/entrypoint.sh
+++ generic/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 cd /home/container
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
 # Advertising Plugin
 if [ "${ADVERTISEMENT_PLUGIN}" == "Enabled" ]; then
@@ -26,6 +27,11 @@ elif [ "${ADVERTISEMENT_PLUGIN}" == "Disabled" ]; then
     # Advertisement Plugin is disabled
     echo -e "\n[+] Advertisement Plugin is disabled"
 
+    # Create 'plugins' directory if it doesn't exist
+    if [ ! -d plugins ]; then
+        mkdir plugins
+    fi
+
     # Remove the Advertisement plugin if it exists
     if [ -f "plugins/TitaniteNode_AdvertisementPE.phar" ]; then
         rm -rf plugins/TitaniteNode_AdvertisementPE.phar
@@ -33,8 +39,8 @@ elif [ "${ADVERTISEMENT_PLUGIN}" == "Disabled" ]; then
 fi
 
 # Replace Startup variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo "customer@titanitenode:~# ${MODIFIED_STARTUP}"
 
 # Run the Server.
-eval ${MODIFIED_STARTUP}
+eval "${MODIFIED_STARTUP}"
