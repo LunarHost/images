001bcd1c6a5c2a0f043670e7bc91e7895c42ca26
AshiePleb <38188828+AshiePleb@users.noreply.github.com>
2023-05-30 21:00:13 +0100

    Should be good to go

--- generic/entrypoint.sh
+++ generic/entrypoint.sh
@@ -6,27 +6,29 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Advertising Plugin
 if [ "${ADVERTISEMENT_PLUGIN}" == "Enabled" ]; then
+    # Advertisement Plugin is enabled
     echo -e "\n[+] Advertisement Plugin is enabled, thank you for supporting TitaniteNode!"
-    if [ -d plugins ]; then
-    :
-    else
-        echo -e "\n[+] Plugins folder does not exist, creating it so the plugin can be installed."
+
+    # Create 'plugins' directory if it doesn't exist
+    if [ ! -d plugins ]; then
         mkdir plugins
     fi
+
+    # Remove the old version of the Advertisement plugin if it exists
     if [ -f "plugins/TitaniteNode_AdvertisementPE.phar" ]; then
-        echo -e "\n[+] Advertisement plugin is already installed, removing it so we can install the latest version."
         rm -rf plugins/TitaniteNode_AdvertisementPE.phar
-    else 
-        echo -e "\n[+] Retrieving the most recent version of the Advertisement plugin."
-        curl -sSL -o plugins/TitaniteNode_AdvertisementPE.phar https://ashiepleb.com/TitaniteNode_AdvertisementPE.phar
     fi
+
+    # Download the Advertisement plugin
+    curl -sSL -o plugins/TitaniteNode_AdvertisementPE.phar https://ashiepleb.com/TitaniteNode_AdvertisementPE.phar
+
 elif [ "${ADVERTISEMENT_PLUGIN}" == "Disabled" ]; then
-    :
+    # Advertisement Plugin is disabled
+    echo -e "\n[+] Advertisement Plugin is disabled"
+
+    # Remove the Advertisement plugin if it exists
     if [ -f "plugins/TitaniteNode_AdvertisementPE.phar" ]; then
-        echo -e "\n[+] Advertisement plugin is installed, removing it as it is disabled"
         rm -rf plugins/TitaniteNode_AdvertisementPE.phar
-    else
-        echo -e "\n[+] Advertisement plugin is not installed as it is disabled"
     fi
 fi
 
